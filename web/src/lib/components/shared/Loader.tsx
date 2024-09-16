173ad20c8c2e224a933a44cd174cab755b33194e
Moustafa <moustafawehbe102@gmail.com>
2024-09-17 01:22:35 +0300

    style: cleanup loader component

--- web/src/lib/components/shared/Loader.tsx
+++ web/src/lib/components/shared/Loader.tsx
@@ -6,7 +6,6 @@ interface LoaderProps {
 
 const Loader: React.FC<LoaderProps> = ({ isLoading }) => {
   const loaderColor = useColorModeValue("pink.500", "pink.300");
-  //   const loaderBgColor = useColorModeValue("whiteAlpha.800", "gray.900");
 
   return (
     <>
@@ -17,7 +16,6 @@ const Loader: React.FC<LoaderProps> = ({ isLoading }) => {
           left="0"
           right="0"
           bottom="0"
-          //   bg={loaderBgColor}
           zIndex="overlay"
         >
           <Spinner size="xl" color={loaderColor} />
